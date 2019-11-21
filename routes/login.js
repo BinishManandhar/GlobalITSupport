539e84b6f899df18012586e1d49c1801b964e910
Binish Manandhar <binish.manandhar23@gmail.com>
2019-11-21 22:36:14 +0545

    Logout Feature

--- routes/login.js
+++ routes/login.js
@@ -3,38 +3,42 @@ var employee = require('../model/employees');
 
 var router = express.Router();
 
-router.get('/',function(req,res){
-    res.render('login',{layout:'nondefault'});
+router.get('/', function (req, res) {
+    res.render('login', { layout: 'nondefault' });
 });
 
-router.get('/signup',function(req,res){
-    res.render('signup',{layout:'nondefault'});
+router.get('/signup', function (req, res) {
+    res.render('signup', { layout: 'nondefault' });
 });
 
-router.post('/',function(req,res,next){
-    employee.authenticate(req,res,function(err,user){
-        if(err || ! user)
+router.get('/logout', function (req, res) {
+    res.render('login', { layout: 'nondefault' });
+});
+
+router.post('/', function (req, res, next) {
+    employee.authenticate(req, res, function (err, user) {
+        if (err || !user)
             res.redirect('/error');
-            // next(err);
+        // next(err);
         else
             res.redirect('/index');
     })
 
 });
 
-router.post('/signup',function(req,res,next){
+router.post('/signup', function (req, res, next) {
 
-    employee.enterNewEmployee(req,res,function(result,err){
-        if(err == null)
+    employee.enterNewEmployee(req, res, function (result, err) {
+        if (err == null)
             res.redirect('/');
         else
             next(err);
     })
 
-    });
+});
 
-router.get('/error',function(req,res,next){
-    res.render('unauthorized',{message:"Username/Password Invalid", layout: 'nondefault' });
+router.get('/error', function (req, res, next) {
+    res.render('unauthorized', { message: "Username/Password Invalid", layout: 'nondefault' });
 })
 
 module.exports = router;
